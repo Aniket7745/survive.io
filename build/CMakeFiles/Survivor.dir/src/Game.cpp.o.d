88e34bc98377b5e5977913d66105596b816d18dc
Aniket7745 <ignoob1000@gmail.com>
2026-07-26 03:32:00 +0530

    Ignore build directory

--- build/CMakeFiles/Survivor.dir/src/Game.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/Survivor.dir/src/Game.cpp.o: \
- /home/aniket/Desktop/c++/survive.io/src/Game.cpp \
- /usr/include/stdc-predef.h \
- /home/aniket/Desktop/c++/survive.io/include/Game.h /usr/include/raylib.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/16/include/stdarg.h
--- build/CMakeFiles/Survivor.dir/src/Player.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Survivor.dir/src/Player.cpp.o: \
- /home/aniket/Desktop/c++/survive.io/src/Player.cpp \
- /usr/include/stdc-predef.h
