872e3c82ca8161346b0706758750a8d9a6fe04f6
Aniket7745 <ignoob1000@gmail.com>
2026-07-26 03:37:55 +0530

    minor fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,8 @@
+#include "../include/Game.h"
 
-#include "raylib.h"
-
-int main(void) {
-  // Initialization
-
-  const int screenWidth = 800;
-  const int screenHeight = 450;
-
-  InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
-  SetTargetFPS(120); // Set our game to run at 60 frames-per-second
-
-  // Main game loop
-  while (!WindowShouldClose()) // Detect window close button or ESC key
-  {
-    // Update
-
-    // TODO: Update your variables here
-
-    // Draw
-    BeginDrawing();
-
-    ClearBackground(RAYWHITE);
-
-    DrawText("Congrats! You created your first window!", 190, 200, 20,
-             LIGHTGRAY);
-
-    EndDrawing();
-    //----------------------------------------------------------------------------------
-  }
-
-  CloseWindow();
+int main() {
+  Game game;
+  game.Run();
 
   return 0;
 }
